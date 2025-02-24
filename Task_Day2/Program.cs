2fec2784381db4168369196580c7d86fe8df37ae
Somnath Prajapati <gazynova@gmail.com>
2025-02-24 18:43:11 +0530

    Task 2 done

---
+++ Task_Day2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your Basic Salary:\t");
+            double salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Tax Deducted 10% of Basic Salary:\t");
+            double tax = Convert.ToDouble(salary*0.1);
+            Console.WriteLine($"Tax Deducted::\t{tax}");
+            Console.WriteLine($"Write the Performance Rating:\t");
+            double rating = Convert.ToDouble(Console.ReadLine());
+            double Bonus;
+            if (rating >= 8)
+            {
+                Bonus = Convert.ToDouble(salary * 0.2);
+                Console.WriteLine($"Bonus Given::{Bonus}");
+            }
+            else if(rating >= 5)
+            {
+                Bonus = Convert.ToDouble(salary * 0.1);
+                Console.WriteLine($"Bonus Given::{Bonus}");
+            }
+            else
+            {
+                Bonus = 0;
+                Console.WriteLine("No Bonus");
+            }
+
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($"Final Salary::{salary + Bonus - tax}");
+
+
+        }
+    }
+}
